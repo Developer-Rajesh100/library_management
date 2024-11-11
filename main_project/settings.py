7b3f653ed668f1e618d84a3287bfd704cd382df7
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-11 11:34:30 +0530

    ALLOWED_HOSTS changed

--- main_project/settings.py
+++ main_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "library.developer-rajesh.in"]
 
 
 # Application definition
